40736b47482f503ba268a5d34d7f661ee04cef5e
Rada Berar <rada.berar@sotexsolutions.com>
2025-10-06 10:50:45 +0200

    Started website for backup

---
+++ backup_sites/settings.py
@@ -0,0 +1,11 @@
+# Admin account
+USER = "admin"
+PASS = "admin123"
+
+# How many backups to keep
+KEEP = 7
+
+APP_TITLE = "Backup Portal"
+APP_SECRET_KEY = 'BackupSuperSecretKey123'        # Used for flask secure communication
+MAX_COOKIE_AGE = 60 * 60 * 2
+DB_NAME = "database.db"
